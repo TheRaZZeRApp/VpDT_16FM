84336f0b137ddf1afa02ed88bf5f9b81d45e4f37
Sonny <rezzer101@googlemail.com>
2016-02-29 09:12:19 +0100

    [Fix] Design

--- src/de/therazzerapp/vpdt_16fm/cryptographics/CUtils.java
+++ src/de/therazzerapp/vpdt_16fm/cryptographics/CUtils.java
@@ -17,7 +17,11 @@ public class CUtils {
      */
     public static char[] charSet= {'~','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9','0','_','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','!','\'','§','$','%','&','/','(',')','=','?',',',';','.','ö','Ö','ü','Ü','ä','Ä','*','-',':',' ','>','<'};
 
+    /**
+     * The global Random instance
+     */
     private static Random random = new Random();
+
     /**
      * Gets added to the dividend's last line until its the same size as the password.
      * Since 0.0.2 its always a random symbol.
